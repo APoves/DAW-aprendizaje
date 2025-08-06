03846ce308ca5460fb430dcf8e3f04358139ef88
MarinaPoves <anirampoves@gmail.com>
2025-08-06 18:19:51 +0200

    update index.html and InvoiceApp.jsx of the proyect invoice

--- Invoice system (React)/src/components/InvoiceApp.jsx
+++ Invoice system (React)/src/components/InvoiceApp.jsx
@@ -12,45 +12,61 @@ export const InvoiceApp = () => {
     
     return(
         <>
-            <h1>Ejemplo Factura:</h1>
-            <ul>
-                <li> Id: { id }</li>
-                <li> Name: { name }</li>
-            </ul>
+            <div className ="container">
 
-            <h3>Datos del cliente:</h3>
-            <ul>
-                <li>{ nameClient } { lastName }</li>                
-                <li>{ country }</li>
-                <li>{ city }</li>
-                <li>{ street } { number } </li>
-                <li>{ postalCode }</li>
-            </ul>
+                <div className="card my-4">
 
-            <h3>Datos de la empresa:</h3>
-            <ul>
-                <li> { company.name }</li>
-                <li> { company.fiscalNumber }</li>
-            </ul>
+                    <div className="card-header fs-5 fw-semibold ">
+                        Ejemplo Factura:
+                    </div>
+                    
+                    <div className="card-body">
+                        <ul className = "list-group">
+                            <li className = "list-group-item"> Id: { id }</li>
+                            <li className = "list-group-item"> Name: { name }</li>
+                        </ul>
 
-            <h4>Productos de la factura:</h4>
-            <table>
-                <thead>
-                    <tr>
-                    <th>Producto</th>
-                    <th>Precio</th>
-                    <th>Cantidad</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    { items.map(({id, product, price, quantity}) => (<tr key = { id }>
-                            <td>{ product }</td>
-                            <td>{ price }</td>
-                            <td>{ quantity }</td>
-                        </tr>
-                        ))}
-                </tbody>
-            </table>
+                        <div className="row my-3">
+                            <div className="col">
+                                <h5>Datos del cliente:</h5>
+                                <ul className="list-group">
+                                    <li className="list-group-item list-group-item-primary">{ nameClient } { lastName }</li>                
+                                    <li className="list-group-item">{ city } / { country }</li>
+                                    <li className="list-group-item">{ street }, { number } </li>
+                                    <li className="list-group-item">{ postalCode }</li>
+                                </ul>
+                            </div>
+
+                            <div className="col">
+                                <h5>Datos de la empresa:</h5>
+                                <ul className="list-group">
+                                    <li className="list-group-item list-group-item-primary"> { company.name }</li>
+                                    <li className="list-group-item"> { company.fiscalNumber }</li>
+                                </ul>
+                            </div>
+                        </div>
+
+                        <h5>Productos de la factura:</h5>
+                        <table className ="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Producto</th>
+                                    <th>Precio</th>
+                                    <th>Cantidad</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                { items.map(({id, product, price, quantity}) => (<tr key = { id }>
+                                    <td>{ product }</td>
+                                    <td>{ price }</td>
+                                    <td>{ quantity }</td>
+                                </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
