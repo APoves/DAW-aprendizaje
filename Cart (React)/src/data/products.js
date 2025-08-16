43e1ea9de83db7cd0d20a529494ff1acb4fb31f1
MarinaPoves <anirampoves@gmail.com>
2025-08-16 12:16:08 +0200

    fix NaN, fix name

--- Cart (React)/src/data/products.js
+++ Cart (React)/src/data/products.js
@@ -3,41 +3,41 @@ export const products = [
         id: 1,
         name: 'La sombra del viento',
         description: 'Carlos Ruiz Zafón',
-        precio: 22.90
+        price: 22.90
     },
 
     {
         id: 2,
         name: 'Diez negritos',
         description: 'Agatha Christie',
-        precio: 19.95
+        price: 19.95
     },
 
     {
         id: 3,
         name: 'El asesinato de Pitágoras',
         description: 'Marcos Chicot',
-        precio: 23.50
+        price: 23.50
     },
 
     {
         id: 4,
         name: 'El cuarto mono',
         description: 'J.D. Barker',
-        precio: 21.90
+        price: 21.90
     },
 
     {
         id: 5,
         name: 'Los pilares de la tierra',
         description: 'Ken Follet',
-        precio: 18.75
+        price: 18.75
     },
 
     {
         id: 6,
         name: 'El imperio final',
         description: 'Brandon Sanderson',
-        precio: 21.90
+        price: 21.90
     }
 ]
