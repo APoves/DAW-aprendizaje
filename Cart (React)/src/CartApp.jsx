7b9c0150675d3a8ef3a4bbe215b0bbab1c83442b
MarinaPoves <anirampoves@gmail.com>
2025-09-05 20:53:11 +0200

    finish Routes and finish proyect

--- Cart (React)/src/CartApp.jsx
+++ Cart (React)/src/CartApp.jsx
@@ -1,8 +1,6 @@
-import { Navigate, Route, Routes } from "react-router-dom"
-import { CartDetails } from "./components/CartDetails"
-import { CatalogDetails } from "./components/CatalogDetails"
+import { NavBar } from "./components/NavBar";
 import { useItemsCart } from "./hooks/useItemsCart"
-import { Navbar } from "./components/Navbar"
+import { CartRoutes } from "./routes/CartRoutes";
 
 export const CartApp = () => {
 
@@ -14,6 +12,11 @@ export const CartApp = () => {
 
       <div className="container my-4">
         <h3>Cart App</h3>
+        <CartRoutes 
+          cartItems = { cartItems }
+          handlerAddProductCart = { handlerAddProductCart }
+          handlerDeleteProductCart = { handlerDeleteProductCart }
+        />
       </div>
     </>
   )
