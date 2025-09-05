e13300b95386a3b2c2897626f321001f9d563cab
MarinaPoves <anirampoves@gmail.com>
2025-09-05 20:26:36 +0200

    add CartRoutes.jsx

--- Cart (React)/src/CartApp.jsx
+++ Cart (React)/src/CartApp.jsx
@@ -13,31 +13,7 @@ export const CartApp = () => {
       <Navbar />
 
       <div className="container my-4">
-
         <h3>Cart App</h3>
-
-        <Routes>
-
-          <Route 
-            path = "catalog"
-            element = { <CatalogDetails handler = { handlerAddProductCart } /> } /* Agrega nuevos productos */
-          />
-
-          <Route path = "cart" element = {(
-            cartItems?.length <= 0 ?
-              <div className="alert alert-warning my-4 w-50 text-center"> No hay productos en el carro de compra.</div>
-              :
-            (
-              <div className="my-4 w-50">
-                <CartDetails items = { cartItems } handlerDelete = {handlerDeleteProductCart} />
-              </div>
-            )
-          )} />
-
-          <Route path = "/" element = { <Navigate to = {'/catalog'} />} />
-
-        </Routes>
-
       </div>
     </>
   )
