411c13dbccc551494d1b5ff57d196379b7f3be3e
MarinaPoves <anirampoves@gmail.com>
2025-09-07 19:52:46 +0200

    add form to UserForm

--- UserHubLogin (React)/src/components/UserForm.jsx
+++ UserHubLogin (React)/src/components/UserForm.jsx
@@ -1,13 +1,56 @@
+import { useState } from "react";
+
+
+const initialUSerForm = {
+    username: '',
+    password: '',
+    email: '',
+}
+
 export const UserForm = () => {
 
-        return (
+    const [ userForm, setUserForm ] = useState (initialUSerForm);
 
-        <>
-            <p>Formulario de usuario</p>
-            <table>
+    const { username, password, email } = userForm;
 
-            </table>
-        </>
+    const onInputChange = ({target })=> {
+        // console.log(target.value)
+        const { name, value } = target;
+        setUserForm ({
+            ...userForm,
+            [ name ] : value,
+        })
+    }
+    
+    return (
+
+            <form>
+                <input 
+                className= "form-control my-3 w-75"
+                placeholder ="Username"
+                name = "username"
+                value = { username }
+                onChange = { onInputChange } />
+                <input 
+                className= "form-control my-3 w-75"
+                placeholder ="Password"
+                type = "password"
+                name = "password"
+                value = { password }
+                onChange = { onInputChange } />
+                <input 
+                className= "form-control my-3 w-75"
+                placeholder ="Email"
+                name = "email" 
+                value = { email }
+                onChange = { onInputChange } />
+                <button
+                className = "btn btn-primary"
+                type= "Submit">
+                    Crear usuario
+                </button>
+            </form>
+
 
     )
 }
