658861a4f2425195fe6f32315ef9759a43d25c6e
MarinaPoves <anirampoves@gmail.com>
2025-09-12 19:09:21 +0200

    add login page

--- UserHubLogin (React)/src/main.jsx
+++ UserHubLogin (React)/src/main.jsx
@@ -1,10 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import { UsersApp } from './UsersApp.jsx'
+import { LoginPage } from './auth/pages/LoginPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <UsersApp />
+    <LoginPage />
   </StrictMode>,
 )
