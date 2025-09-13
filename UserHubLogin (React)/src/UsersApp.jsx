2b9f9327c350d53e19d082666a8576a803fdef68
MarinaPoves <anirampoves@gmail.com>
2025-09-13 18:39:12 +0200

    add routes

--- UserHubLogin (React)/src/UsersApp.jsx
+++ UserHubLogin (React)/src/UsersApp.jsx
@@ -3,22 +3,30 @@ import { LoginPage } from "./auth/pages/LoginPage";
 import { UsersPage } from "./pages/UsersPage";
 import { Navbar } from "./components/layout/Navbar";
 import { useAuthentication } from "./auth/hooks/UseAuthentication";
+import { Route, Routes, Navigate } from "react-router-dom";
+import { UserRoutes } from "./routes/UserRoutes";
 
 export const UsersApp = () => {
 
     const { login, handlerLogin, handlerLogout } = useAuthentication();
 
     return (
-        <>
-        { login.isAuthenticated
-            ? (
-                <>  
-                    <Navbar login = { login } handlerLogout = { handlerLogout }/>      
-                    <UsersPage />
-                </>
-            )
-            : <LoginPage handlerLogin = { handlerLogin } />            
+        <Routes>
+        { 
+            login.isAuthenticated
+                ? (
+                    <Route path = '/*' element = { <UserRoutes 
+                        login = { login }
+                        handlerLogout = { handlerLogout }
+                    /> } />
+                )
+                : <>
+                    <Route path = '/login' 
+                    element = { <LoginPage 
+                    handlerLogin = { handlerLogin } />} />
+                    <Route path = '/*' element = { <Navigate to = "/login" /> } />
+                </>            
         }
-        </>
+        </Routes>
     );
 }
