3312a0c91a848269bfb15731a67357564e5c8fdb
MarinaPoves <anirampoves@gmail.com>
2025-09-23 20:31:54 +0200

    create new proyect backend_userhublogin

---
+++ backend_userhublogin/src/main/java/com/aurora/backend/userhublogin/backend_userhublogin/BackendUserhubloginApplication.java
@@ -0,0 +1,13 @@
+package com.aurora.backend.userhublogin.backend_userhublogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendUserhubloginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendUserhubloginApplication.class, args);
+	}
+
+}
---
+++ backend_userhublogin/src/main/java/com/aurora/backend/userhublogin/backend_userhublogin/models/entities/User.java
@@ -0,0 +1,5 @@
+package com.aurora.backend.userhublogin.backend_userhublogin.models.entities;
+
+public class User {
+
+}
