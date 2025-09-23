e47cecb261ab601e771badbd77c8798cef5c8981
MarinaPoves <anirampoves@gmail.com>
2025-09-23 14:00:00 +0200

    add items into database

---
+++ backend-cartapp/src/main/resources/import.sql
@@ -0,0 +1,11 @@
+INSERT INTO products (name, description, price) VALUES ('Teclado Mecánico RGB', 'Teclado Mecánico con luces RGB switches cherry red', 1000);
+ 
+INSERT INTO products (name, description, price) VALUES ('Samsung Smart TV 55', 'Color optimizado para una imagen vibrante y realista gracias al  Crystal Processor 4K', 3000);
+ 
+INSERT INTO products (name, description, price) VALUES ('Auriculares Bluetooth Sony', 'Auriculares Bluetooth para smartphone', 770);
+ 
+INSERT INTO products (name, description, price) VALUES ('Memoria Corsair 16GB DDR5', 'Memoria RAM optimizada para Juegos', 3700);
+ 
+INSERT INTO products (name, description, price) VALUES ('Asus Nvidia RTX 4080TI', 'Tarjeta Gráfica para juegos en 4k', 5000);
+ 
+INSERT INTO products (name, description, price) VALUES ('CPU Intel Core i7-12700K', 'Alder Lake de 12 cores optimizados para multitareas', 5000);
