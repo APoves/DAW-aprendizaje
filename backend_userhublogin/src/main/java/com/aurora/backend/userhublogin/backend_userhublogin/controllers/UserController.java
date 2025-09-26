c47e6486542f2f058b4c53f8f31a6320b04a2848
MarinaPoves <anirampoves@gmail.com>
2025-09-26 20:38:18 +0200

    implemente RestController

---
+++ backend_userhublogin/src/main/java/com/aurora/backend/userhublogin/backend_userhublogin/controllers/UserController.java
@@ -0,0 +1,39 @@
+package com.aurora.backend.userhublogin.backend_userhublogin.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.http.ResponseEntity;
+
+import com.aurora.backend.userhublogin.backend_userhublogin.models.entities.User;
+import com.aurora.backend.userhublogin.backend_userhublogin.services.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @GetMapping
+    public List<User> List(){
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> show(@PathVariable Long id) {
+        Optional<User> userOptional = service.findById(id);
+
+        if(userOptional.isPresent()){
+            //200 OK
+            return ResponseEntity.ok(userOptional.orElseThrow());
+        } 
+        //404 Not Found
+        return ResponseEntity.notFound().build();
+    }
+
+}
--- backend_userhublogin/src/main/java/com/aurora/backend/userhublogin/backend_userhublogin/models/entities/User.java
+++ backend_userhublogin/src/main/java/com/aurora/backend/userhublogin/backend_userhublogin/models/entities/User.java
@@ -3,6 +3,7 @@ package com.aurora.backend.userhublogin.backend_userhublogin.models.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
