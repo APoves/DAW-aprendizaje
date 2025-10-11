8d699c6358c927e0fe8d3bbc03a21725caef1ab8
MarinaPoves <anirampoves@gmail.com>
2025-10-11 18:29:32 +0200

    add dto

--- backend_userhublogin/src/main/java/com/aurora/backend/userhublogin/backend_userhublogin/services/UserService.java
+++ backend_userhublogin/src/main/java/com/aurora/backend/userhublogin/backend_userhublogin/services/UserService.java
@@ -3,17 +3,19 @@ package com.aurora.backend.userhublogin.backend_userhublogin.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.aurora.backend.userhublogin.backend_userhublogin.models.dto.UserDto;
 import com.aurora.backend.userhublogin.backend_userhublogin.models.entities.User;
 import com.aurora.backend.userhublogin.backend_userhublogin.models.request.UserRequest;
 
 public interface UserService {
+    
+    List<UserDto> findAll();
 
-    List<User> findAll();
+    Optional<UserDto> findById(Long id);
 
-    Optional<User> findById(Long id);
+    UserDto save(User user);
 
-    User save(User user);
-    Optional<User> update(UserRequest user, Long id);
+    Optional<UserDto> update(UserRequest user, Long id);
 
     void remove(Long id);
 }
