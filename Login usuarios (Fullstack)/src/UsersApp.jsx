9907dbcda785286db4e240ae8bb0718eecddca91
MarinaPoves <anirampoves@gmail.com>
2025-10-13 18:49:02 +0200

    start Redux

--- Login usuarios (Fullstack)/src/UsersApp.jsx
+++ Login usuarios (Fullstack)/src/UsersApp.jsx
@@ -1,25 +1,12 @@
-import { useContext } from 'react'
-import { AuthContext } from './auth/context/AuthContext'
-import { LoginPage } from "./auth/pages/LoginPage";
-import { Route, Routes, Navigate } from "react-router-dom";
-import { UserRoutes } from "./routes/UserRoutes";
-
+import { Provider } from "react-redux"
+import { AppRoutes } from "./AppRoutes"
+import { store } from "./store/store"
 export const UsersApp = () => {
 
-    const { login } = useContext(AuthContext);
-
     return (
-        <Routes>
-        { 
-            login.isAuthenticated
-                ? (
-                    <Route path = '/*' element = { <UserRoutes /> } />
-                )
-                : <>
-                    <Route path = '/login' element = { <LoginPage />} />
-                    <Route path = '/*' element = { <Navigate to = "/login" /> } />
-                </>            
-        }
-        </Routes>
-    );
+        <Provider store =  { store }>
+            <AppRoutes />
+        </Provider>
+    )
+
 }
